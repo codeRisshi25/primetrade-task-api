7c9d79d12806fb586dd92147ccb17593d4c5c1d3
Risshi25 <risshirajsen@gmail.com>
2026-02-13 21:39:08 +0530

    fix db ssl for render production

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,11 +1,16 @@
-const { Sequelize } = require('sequelize');
-const env = require('./env');
-const logger = require('./logger');
+const { Sequelize } = require("sequelize");
+const env = require("./env");
+const logger = require("./logger");
 
 const sequelize = new Sequelize(env.databaseUrl, {
-  dialect: 'postgres',
+  dialect: "postgres",
   logging: (msg) => logger.debug(msg),
   pool: { max: 10, min: 2, idle: 10000 },
+  ...(env.nodeEnv === "production" && {
+    dialectOptions: {
+      ssl: { require: true, rejectUnauthorized: false },
+    },
+  }),
 });
 
 module.exports = sequelize;
